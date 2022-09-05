a6ed4bd096efcd7d2d0753b9789d5e1c490f9c3d
Conor Burns <ronoc99@hotmail.co.uk>
2022-09-05 16:34:30 +0100

    KeyBoardListener Commit MyMouseListener Commit

--- Tower Defence Game/src/inputs/KeyBoardListener.java
+++ Tower Defence Game/src/inputs/KeyBoardListener.java
@@ -1,5 +1,26 @@
 package inputs;
 
-public class KeyBoardListener {
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class KeyBoardListener implements KeyListener {
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
